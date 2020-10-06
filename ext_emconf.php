f65adf5ea8a38a8dfd479252239faf1dec86d308
Marc Finnern <marc@klickfabrik.net>
2020-10-06 14:04:03 +0200

    Version 1.0.13

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.12',
+  'version' => '1.0.13',
   'constraints' => 
   array (
     'depends' => 
